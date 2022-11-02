85338b9b0a3fe823ae1d72ee083373f31e4b101b
Diogo Mombaque <diogo.mombaque@neobpo.com.br>
2022-11-01 23:02:48 -0300

    Conclusão da primeira aula nlw copa

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "native-base";
+export function SignIn(){
+    return(
+        <Center flex={1} bgColor="warmGray.900">
+            <Text color="white" fontSize={24}> 
+                SigIn
+            </Text>
+        </Center>
+    )
+}
